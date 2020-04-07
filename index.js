3de3a1f025307727a3b55baa0ef9b3832fded234
Marcel Melo <marcel.mdr@gmail.com>
2020-04-07 18:19:23 -0400

    - Added new modules

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-module.exports = function () {
-    return "echo test...";
+const chalk = require('chalk');
+
+const log = (text) => {
+    return console.log('log', text);
 }
+
+const logSuccess = (text) => {
+    return console.log(chalk.inverse.greenBright('success'), text);
+}
+
+const logWarn = (text) => {
+    return console.log(chalk.inverse.magentaBright('warn'), text);
+}
+
+const logInfo = (text) => {
+    return console.log(chalk.inverse.blueBright('info'), text);
+}
+
+const logError = (text) => {
+    return console.log(chalk.inverse.redBright('error'), text);
+}
+
+module.exports = {
+    log,
+    logError,
+    logInfo,
+    logSuccess,
+    logWarn,
+};
